f27fe9b1c8fda58f638fe0ae014d8daa669cf277
Tomoaki Yoshinaga <naruto1439m@gmail.com>
2020-06-24 15:06:25 +0900

    add devise

--- Gemfile
+++ Gemfile
@@ -68,3 +68,4 @@ end
 gem 'html2slim'
 gem 'slim-rails'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#index'
   resources :books do
     member do
